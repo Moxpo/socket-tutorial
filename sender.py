a35713af05cf865e09c2a91e697c1ef38ec13883
Gregor <gregor.fiddes@gmail.com>
2023-05-04 18:43:41 +0100

    test file changed

--- sender.py
+++ sender.py
@@ -4,10 +4,10 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 9999))
 
-file = open("tango.pdf", "rb")
-file_size = os.path.getsize("tango.pdf")
+file = open("test.txt", "rb")
+file_size = os.path.getsize("test.txt")
 
-client.send("recieved_tango.pdf".encode())
+client.send("recieved_test.txt".encode())
 client.send(str(file_size).encode())
 
 data = file.read()

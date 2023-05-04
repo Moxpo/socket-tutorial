9a0a54cbae5e02de7754006d8e9dcd7e887efcc4
Gregor <gregor.fiddes@gmail.com>
2023-05-04 19:17:56 +0100

    almost done, file wrote not correct

--- reciever.py
+++ reciever.py
@@ -11,3 +11,22 @@ file_name = client.recv(1024).decode()
 print(file_name)
 file_size = client.recv(1024).decode()
 print(file_size)
+
+file = open(file_name, "wb")
+
+file_bytes = b""
+
+done = False
+
+while not done:
+    data = client.recv(1024)
+    if file_bytes[-5:] == b"<END>":
+        done = True
+    else:
+        file_bytes += data
+
+file.write(file_bytes)
+
+file.close()
+client.close()
+server.close()
